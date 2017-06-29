de66c2fcda3727bcc874f18cd36ebd51b40628bf
omarRBZ <usa.92slipk@gmail.com>
2017-06-29 00:43:58 -0500

    correcion del for decremento

--- Render.js
+++ Render.js
@@ -3,13 +3,24 @@ const fs = require('fs');
 
 http.createServer(function (req,res) {
   fs.readFile("./index.html",function (err,html) {
+    if (req.url.indexOf("favicon.ico")>0) {return;}
     var html_string=html.toString();
+    var arregloParametro=[],parametros={};
     var variables=html_string.match(/[^\{\}]+(?=\})/g);
     var nombre= "codigo ";
+    if (req.url.indexOf("?")>0) {
+      var urlData=req.url.split("?");
+      var arregloParametro=urlData[1].split("&");
+    }
+    for (var i = arregloParametro.length-1;i>=0 ;i--) {
+      var parametro=arregloParametro[i];
+      var parametroData= parametro.split("=");
+      parametros[parametroData[0]]=parametroData[1];
+    }
 
     for (var i = variables.length-1; i >= 0; i--) {
-      var  value=eval(variables[i]);
-      html_string=html_string.replace("{"+variables[i]+"}",value);
+      var  variable=variables[i];
+      html_string=html_string.replace("{"+variables[i]+"}",parametros[variable]);
 
 
     };
