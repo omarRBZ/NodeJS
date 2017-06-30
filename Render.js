c6c871bcf514cd7f4a4001a1c96c3f12084e83ef
omarRBZ <usa.92slipk@gmail.com>
2017-06-29 23:50:57 -0500

    render modificado con requires

--- Render.js
+++ Render.js
@@ -1,31 +1,20 @@
 const http = require('http');
 const fs = require('fs');
+const parser= require('./paramParse.js');
+const renders = require('./render2.js');
+var p=parser.parse;
+var r=renders.render2;
+
 
 http.createServer(function (req,res) {
   fs.readFile("./index.html",function (err,html) {
     if (req.url.indexOf("favicon.ico")>0) {return;}
     var html_string=html.toString();
-    var arregloParametro=[],parametros={};
-    var variables=html_string.match(/[^\{\}]+(?=\})/g);
-    var nombre= "codigo ";
-    if (req.url.indexOf("?")>0) {
-      var urlData=req.url.split("?");
-      var arregloParametro=urlData[1].split("&");
-    }
-    for (var i = arregloParametro.length-1;i>=0 ;i--) {
-      var parametro=arregloParametro[i];
-      var parametroData= parametro.split("=");
-      parametros[parametroData[0]]=parametroData[1];
-    }
-
-    for (var i = variables.length-1; i >= 0; i--) {
-      var  variable=variables[i];
-      html_string=html_string.replace("{"+variables[i]+"}",parametros[variable]);
+    var parametros=p(req);
 
 
-    };
   res.writeHead(200,{"content-Type":"text/html"})
-  res.write(html_string);
+  res.write(r(html_string,parametros));
   res.end();
   });
 
