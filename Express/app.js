18b63d485a8ce670fabfc2df93773326500801c5
omarRBZ <usa.92slipk@gmail.com>
2017-07-01 22:15:34 -0500

    creacion de vistas con layaout con jade y form para login

--- Express/app.js
+++ Express/app.js
@@ -2,8 +2,15 @@ const express = require('express');
 var app=express();
 app.set("view engine","jade");
 app.get("/",function (req,res) {
-  res.render("index",{hola:"hola omar"});
-  res.end();
+  res.render("index");
+});
+app.post("/",function (req,res) {
+  res.render("form");
 
 });
+app.get("/segunda",function (req,res) {
+  res.render("segunda");
+
+});
+
 app.listen(8080);
